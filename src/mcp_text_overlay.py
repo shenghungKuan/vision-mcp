8fc9415b12ff20a89274a9443221b1aa1ceb4848
marcus0329 <skuan@dons.usfca.edu>
2025-04-29 22:23:13 -0700

    Clean up

--- src/mcp_text_overlay.py
+++ src/mcp_text_overlay.py
@@ -1,12 +1,8 @@
 import os
-import sys
 import requests
 from typing import Any
-import httpx
 from mcp.server.fastmcp import FastMCP
 from dotenv import load_dotenv
-import logging
-logger = logging.getLogger(__name__)
 
 load_dotenv()
 
@@ -26,9 +22,8 @@ async def text_overlay(blend_mode='normal', image_url: str="", text=""):
         image_url (str): The URL of the image to overlay text on.
         text (str): The text to overlay on the image.
     Returns:
-        Any: The response from the Segmind API.
+        Any: The binary of the processed image.
     """
-    logger.info("Getting into the function")
     data = {
         "blend_mode": blend_mode,
         "image_url": image_url,
@@ -37,11 +32,8 @@ async def text_overlay(blend_mode='normal', image_url: str="", text=""):
     headers = {'x-api-key': api_key}
 
     response = requests.post(url, json=data, headers=headers)
-    print(response.content)
     return response.content
 
 if __name__ == "__main__":
     # Initialize and run the server
-    logging.basicConfig(filename='./mcp.log', level=logging.INFO)
-    logger.info("Starting the server")
     mcp.run(transport='stdio')
